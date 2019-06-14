61949eab4f1f69874a8e24e881fcd756d2eaf4d3
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2019-06-14 21:08:05 +0430

    if sanackbar/index.js

--- src/component/snackbar/index.js
+++ src/component/snackbar/index.js
@@ -4,17 +4,18 @@ import React, { useState } from 'react'
 const SnackBar = (props) => {
     console.log(props.setting.styles);
     const [isOpen, setIsOpen] = useState(true);
+    console.log(isOpen)
     const showSnackBar = () => {
         return (
-            <h1 className="snk" style ={props.setting.styles} >  {props.setting.text}</h1>
+            <h1 className={ isOpen ? "show" : "hide"} style ={props.setting.styles} >  {props.setting.text}</h1>
         )
     }
 
     const openSnackBar = () => {
         if (isOpen) {
-            // setTimeout(function () {
-            //             //      setIsOpen(false)
-            //             // }, props.setting.duration)
+            setTimeout(function () {
+                         setIsOpen(false)
+                       }, props.setting.duration)
             return showSnackBar()
         }
     }
